5c78818ab3127fa8854e9e4930260bedb2a7f85f
Ikimonogatari <tuvshuutuvshinjargal21@gmail.com>
2022-11-17 20:16:53 +0800

    Added getBalance, updating functions to contract & added contract tests

--- ardmoney-staking-reward-automation-contract/contracts/StakingRewardAutomation.sol
+++ ardmoney-staking-reward-automation-contract/contracts/StakingRewardAutomation.sol
@@ -2,28 +2,33 @@
 pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./interfaces/IArdMoneyStakingContract.sol";
 
-contract StakingRewardAutomation is AccessControl {
+contract StakingRewardAutomation is AccessControl, Pausable {
+  bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
   IArdMoneyStakingContract public stakingContract;
   IERC20 public ardmToken;
   address public registryAddress;
   uint public percentage;
+  uint public contractBalance;
 
   event SentReward(address indexed stakingAddress, uint amount);
-  // TODO stakingContractUpdated , ardmTokenUpdated , registeryAddressUpdated Events
-
-  constructor(address ardmAddress, address _stakingContract,address _registeryAddress , uint _percentage) {
+  event stakingContractUpdated(address indexed stakingContractUpdated);
+  event ardmTokenUpdated(address indexed ardmTokenUpdated);
+  event registryAddressUpdated(address indexed registryAddress);
+  event percentageUpdated(address indexed percentageUpdated);
+  constructor(address ardmAddress, address _stakingContract,address _registryAddress , uint _percentage) {
     ardmToken = IERC20(ardmAddress);
     stakingContract = IArdMoneyStakingContract(_stakingContract);
-    registryAddress = _registeryAddress;
+    registryAddress = _registryAddress;
     percentage = _percentage;
 
     _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    _grantRole(PAUSER_ROLE, msg.sender);
   }
-
-  function sendReward() external onlyRegistery() {
+  function sendReward() external onlyRegistry() {
     uint totalStakingARDM = stakingContract.getTotalLockedARDM();
     uint _amount = (totalStakingARDM * percentage) / 100000000000000000000;
 
@@ -48,17 +53,47 @@ contract StakingRewardAutomation is AccessControl {
     uint _amount = (totalStakingARDM * percentage) / 100000000000000000000;
     return _amount;
   }
-
+  // Added getBalance function
+  function getBalance() external view returns(uint){
+    IArdMoneyStakingContract instance = IArdMoneyStakingContract(stakingContract);
+    contractBalance == instance.ardmBalanceOf(address(this));
+    return contractBalance;
+  }
   function updatePercentage(uint _percentage) external onlyRole(DEFAULT_ADMIN_ROLE){
     percentage = _percentage;
   }
+  function updateStakingContract(address _stakingContract) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    stakingContract = IArdMoneyStakingContract(_stakingContract);
+  }
 
-  // TODO updateStakingContract , updateARDMToken , updateRegisteryAddress functions
-  // TODO fire events
+  function updateARDMToken(address _ardmToken) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    ardmToken = IERC20(_ardmToken);
+  }
 
-  modifier onlyRegistery(){
+  function updateRegistryAddress(address _registryAddress) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    registryAddress = _registryAddress;
+  }
+
+  modifier onlyRegistry(){
     require(msg.sender == registryAddress,"NOT REGISTERY");
     _;
   }
+
+  function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+  function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+  // override, super.beforeTokenTransfer removed cause of error
+
+  // function _beforeTokenTransfer(address from, address to, uint256 amount)
+  //       internal
+  //       whenNotPaused
+        
+  // {
+  //     _beforeTokenTransfer(from, to, amount);
+  // }
 
 }
