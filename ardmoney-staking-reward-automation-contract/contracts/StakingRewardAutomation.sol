5a5a4e570f8e4a85027302901b5baeab4a7081e9
Ikimonogatari <tuvshuutuvshinjargal21@gmail.com>
2022-12-01 16:27:07 +0800

    Chainlink Automation Fixed

--- ardmoney-staking-reward-automation-contract/contracts/StakingRewardAutomation.sol
+++ ardmoney-staking-reward-automation-contract/contracts/StakingRewardAutomation.sol
@@ -12,7 +12,6 @@ contract StakingRewardAutomation is AccessControl, Pausable {
   IERC20 public ardmToken;
   address public registryAddress;
   uint public percentage;
-  uint public contractBalance;
 
   event SentReward(address indexed stakingAddress, uint amount);
   event stakingContractUpdated(address indexed stakingContractUpdated);
@@ -24,24 +23,24 @@ contract StakingRewardAutomation is AccessControl, Pausable {
     stakingContract = IArdMoneyStakingContract(_stakingContract);
     registryAddress = _registryAddress;
     percentage = _percentage;
-
     _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     _grantRole(PAUSER_ROLE, msg.sender);
   }
-  function sendReward() external onlyRegistry() {
+  
+  function sendReward() external onlyRegistry whenNotPaused{
     uint totalStakingARDM = stakingContract.getTotalLockedARDM();
     uint _amount = (totalStakingARDM * percentage) / 100000000000000000000;
-
+    ardmToken.approve(address(this), _amount);
     bool result = ardmToken.transferFrom(address(this),address(stakingContract),_amount);
     require(result,"TRANSFER FAILED");
 
     emit SentReward(address(stakingContract),_amount);
   }
 
-  function sendRewardAdmin() external onlyRole(DEFAULT_ADMIN_ROLE) {
+  function sendRewardAdmin() external onlyRole(DEFAULT_ADMIN_ROLE) whenNotPaused {
     uint totalStakingARDM = stakingContract.getTotalLockedARDM();
     uint _amount = (totalStakingARDM * percentage) / 100000000000000000000;
-
+    ardmToken.approve(address(this), _amount);
     bool result = ardmToken.transferFrom(address(this),address(stakingContract),_amount);
     require(result,"TRANSFER FAILED");
 
@@ -53,15 +52,15 @@ contract StakingRewardAutomation is AccessControl, Pausable {
     uint _amount = (totalStakingARDM * percentage) / 100000000000000000000;
     return _amount;
   }
-  // Added getBalance function
+
   function getBalance() external view returns(uint){
-    IArdMoneyStakingContract instance = IArdMoneyStakingContract(stakingContract);
-    contractBalance == instance.ardmBalanceOf(address(this));
-    return contractBalance;
+    return IERC20(address(ardmToken)).balanceOf(address(this)); 
   }
+
   function updatePercentage(uint _percentage) external onlyRole(DEFAULT_ADMIN_ROLE){
     percentage = _percentage;
   }
+
   function updateStakingContract(address _stakingContract) external onlyRole(DEFAULT_ADMIN_ROLE) {
     stakingContract = IArdMoneyStakingContract(_stakingContract);
   }
@@ -75,7 +74,7 @@ contract StakingRewardAutomation is AccessControl, Pausable {
   }
 
   modifier onlyRegistry(){
-    require(msg.sender == registryAddress,"NOT REGISTERY");
+    require(msg.sender == registryAddress,"NOT REGISTRY");
     _;
   }
 
@@ -88,12 +87,11 @@ contract StakingRewardAutomation is AccessControl, Pausable {
     }
   // override, super.beforeTokenTransfer removed cause of error
 
-  // function _beforeTokenTransfer(address from, address to, uint256 amount)
-  //       internal
-  //       whenNotPaused
+  function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
         
-  // {
-  //     _beforeTokenTransfer(from, to, amount);
-  // }
-
+  {
+      _beforeTokenTransfer(from, to, amount);
+  }
 }
